7d850389daf766222e8cfa5df7eb440f54fd62f1
nevmerzhitskaya <lamara@tut.by>
2023-06-13 17:47:19 +0200

    feat: create copy folder function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
+import { opendir, copyFile, mkdir } from 'node:fs/promises';
+import path from 'path';
+import { pathConverter } from '../helpers/pathConverter.js';
+import ErrorsMessage from '../helpers/errors.js';
+
+const pathSource = pathConverter('fs', 'files');
+const pathCopy = pathConverter('fs', 'files_copy');
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        const dir = await opendir(pathSource);
+
+        await mkdir(pathCopy);
+
+        for await (const dirent of dir) {
+            const file = path.resolve(pathSource, dirent.name);
+            const fileCopy = path.resolve(pathCopy, dirent.name);
+
+            await copyFile(file, fileCopy);
+        }
+    } catch (error) {
+        throw new Error(ErrorsMessage.FS_OPERATION_FAILED);
+    }
 };
 
 await copy();
