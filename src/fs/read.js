7ba024321dca0544d23199e9bc7e7bc23a9e1022
nevmerzhitskaya <lamara@tut.by>
2023-06-13 18:40:37 +0200

    feat: add read file function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import { pathConverter } from '../helpers/pathConverter.js';
+import { readFile } from 'node:fs/promises';
+
+const path = pathConverter('fs', 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+
+    try {
+        const contents = await readFile(path, { encoding: 'utf8' });
+        console.log(contents);
+    } catch (err) {
+        throw new Error(ErrorsMessage.FS_OPERATION_FAILED);
+    }
 };
 
 await read();
