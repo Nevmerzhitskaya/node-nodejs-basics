f3d0991351670c4cb78ab7ca7c05471d75d8b657
nevmerzhitskaya <lamara@tut.by>
2023-06-13 18:02:32 +0200

    feat: add function that deletes file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import { unlink } from 'node:fs/promises';
+import { pathConverter } from '../helpers/pathConverter.js';
+import ErrorsMessage from '../helpers/errors.js';
+
+const path = pathConverter('fs', 'files', 'fileToRemove.txt');
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        await unlink(path); 
+    } catch (error) {
+        throw new Error(ErrorsMessage.FS_OPERATION_FAILED);        
+    }
 };
 
 await remove();
