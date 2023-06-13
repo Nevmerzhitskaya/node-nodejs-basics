592c8f919d3287dbbad9d3113f5b498cd67425a4
nevmerzhitskaya <lamara@tut.by>
2023-06-13 18:19:08 +0200

    feat: add list files from folder function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import { pathConverter } from '../helpers/pathConverter.js';
+import { readdir } from 'node:fs/promises';
+
+const pathSource = pathConverter('fs', 'files');
+
 const list = async () => {
-    // Write your code here 
+    try {
+        const dir = await readdir(pathSource);
+        console.table(dir);
+    } catch (error) {
+        throw new Error(ErrorsMessage.FS_OPERATION_FAILED);
+    }
 };
 
 await list();
