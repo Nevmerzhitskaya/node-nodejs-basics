713c01370296c2f0b11b5889ac007be074283ccf
nevmerzhitskaya <lamara@tut.by>
2023-06-13 18:47:50 +0200

    feat: add function that parses environment variables

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+    let envString = '';
+
+    for (const key in process.env) {
+        if (key.startsWith('RSS_')) {
+            envString += (envString != '') ? `; ` : ``;
+            envString += `${key}=${process.env[key]}`;
+        }
+    }
+
+    console.log(envString);
 };
 
 parseEnv();
