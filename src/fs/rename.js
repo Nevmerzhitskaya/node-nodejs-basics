75b099343218db746e0961a0858ce82533cc1685
nevmerzhitskaya <lamara@tut.by>
2023-06-13 17:57:12 +0200

    feat: add rename file function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,15 @@
+import { rename as renameFile } from 'node:fs/promises';
+import { pathConverter } from '../helpers/pathConverter.js';
+
+const oldPath = pathConverter('fs', 'files', 'wrongFilename.txt');
+const newPath = pathConverter('fs', 'files', 'properFilename.md');
+
 const rename = async () => {
-    // Write your code here 
+    try {
+        await renameFile(oldPath, newPath);
+    } catch (error) {
+        throw new Error(ErrorsMessage.FS_OPERATION_FAILED);
+    }
 };
 
 await rename();
