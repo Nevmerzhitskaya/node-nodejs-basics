099dbcba6c7f869a969e113eda51cc0ca4360022
nevmerzhitskaya <lamara@tut.by>
2023-06-14 11:59:41 +0200

    feat: add function for compressing file using zlib and streams

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,15 @@
+import { createGzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pathConverter } from '../helpers/pathConverter.js';
+
+const sourcePath = pathConverter('zip','files','fileToCompress.txt');
+const archivePath = pathConverter('zip','files','archive.gz');
+const gzip = createGzip();
+const source = createReadStream(sourcePath);
+const archive = createWriteStream(archivePath);
+
 const compress = async () => {
-    // Write your code here 
+    source.pipe(gzip).pipe(archive);
 };
 
 await compress();
