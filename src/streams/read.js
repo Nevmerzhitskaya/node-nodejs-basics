092daf4f6a3d7a95c6d54729ebfa9cc26420cb75
nevmerzhitskaya <lamara@tut.by>
2023-06-14 11:37:53 +0200

    feat: add function for reading file using stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { createReadStream } from 'node:fs';
+import { pathConverter } from '../helpers/pathConverter.js';
+
+const filePath = pathConverter('streams','files','fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    const readStream = createReadStream(filePath).pipe(process.stdout);
+
+    readStream.on('error', function (err) {
+        console.log(err);
+    });
 };
 
 await read();
