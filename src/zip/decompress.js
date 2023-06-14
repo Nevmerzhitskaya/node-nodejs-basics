933e9ec4a7145065596ff5ae8617c11b60421fd6
nevmerzhitskaya <lamara@tut.by>
2023-06-14 12:03:59 +0200

    feat: add function for decompressing archive

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,15 @@
+import { createUnzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pathConverter } from '../helpers/pathConverter.js';
+
+const sourcePath = pathConverter('zip','files','archive.gz');
+const archivePath = pathConverter('zip','files','fileToCompress.txt');
+const unzip = createUnzip();
+const source = createReadStream(sourcePath);
+const archive = createWriteStream(archivePath);
+
 const decompress = async () => {
-    // Write your code here 
+    source.pipe(unzip).pipe(archive);
 };
 
 await decompress();
