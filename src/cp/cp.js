d46b47d7f532b2b029007e358151b6e5470cfa25
nevmerzhitskaya <lamara@tut.by>
2023-06-14 12:18:28 +0200

    feat: add function for working with Child Processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
+import { spawn } from 'node:child_process';
+import { pathConverter } from '../helpers/pathConverter.js';
+
+const filePath = pathConverter('cp', 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    spawn('node', [filePath, ...args], {
+        stdio: [process.stdin, process.stdout, process.stderr]
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['arg1', 'arg2', 'arg3']);
