6e3ceee31a6170d4792def418d1d8b1bfe18c52d
nevmerzhitskaya <lamara@tut.by>
2023-06-14 11:41:38 +0200

    feat: add function for writing data into file using stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import { createWriteStream } from 'node:fs';
+import { pathConverter } from '../helpers/pathConverter.js';
+
+const filePath = pathConverter('streams','files','fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    const writeStream = createWriteStream(filePath);
+    process.stdin.pipe(writeStream);
+
+    writeStream.on('error', function (err) {
+        console.log(err);
+    });
 };
 
 await write();
